79f62bde512f8ecef917fbee9662dfa4acd56b31
Abdelaziz Mahamat Ali <azizmahamat@gmail.com>
2025-01-16 11:46:53 +0100

    Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -12,18 +13,26 @@ const Navbar = () => {
   ];
   const isActiveLink = (href: string) =>
     pathname.replace(/\/$/, "") === href.replace(/\/$/, "");
-  const renderLinks = (classNames: string) => {
+  const renderLinks = (classNames: string) =>
     navLinks.map(({ href, label }) => {
       return (
-        <Link href={href} key={href} className={`btn-sm btn ${classNames}`}>
+        <Link
+          href={href}
+          key={href}
+          className={`btn-sm ${classNames} ${
+            isActiveLink(href) ? "btn-accent" : null
+          }`}
+        >
           {label}
         </Link>
       );
     });
-  };
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="">
+      <div>
+        <div>{renderLinks("btn")}</div>
+      </div>
+      <div></div>
     </div>
   );
 };
